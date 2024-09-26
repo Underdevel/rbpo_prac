d4a12f00018b45bf2d55a4ef78c7515b8233927a
Святослав <slapatyn@gmail.com>
2024-09-26 15:49:36 +0300

    123

---
+++ src/main/java/RU/MTUCI/rbpo_prac/RbpoPracApplication.java
@@ -0,0 +1,13 @@
+package RU.MTUCI.rbpo_prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbpoPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbpoPracApplication.class, args);
+	}
+
+}
