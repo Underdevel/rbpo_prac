2101814e8b2b0250e6e224a6671a6245f5306bca
Святослав <slapatyn@gmail.com>
2024-10-23 11:24:27 +0300

    123

--- src/main/java/RU/MTUCI/rbpo_prac/repository/LicenceRepository.java
+++
@@ -1,9 +0,0 @@
-package RU.MTUCI.rbpo_prac.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import RU.MTUCI.rbpo_prac.model.Licence;
-
-@Repository
-public interface LicenceRepository extends JpaRepository<Licence, Long> {
-}
